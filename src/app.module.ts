0a1c9411a9abe67aa577bd66e1351fdde5f9b01e
Guilherme Leite <leite_guilherme@outlook.com>
2023-04-24 14:01:32 -0300

    to do app completo

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
+import { MongooseModule } from '@nestjs/mongoose'
+
+@Module({
+  imports: [
+    TasksModule,
+    MongooseModule.forRoot('mongodb+srv://admin:adminpass@cluster0.1nfzn8r.mongodb.net/?retryWrites=true&w=majority'),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
